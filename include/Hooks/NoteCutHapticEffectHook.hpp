8d44f1fd4c7f8bc96bfba7d3383eca418bebb89e
Francisco Ribeiro <francisco.ribeiro@ua.pt>
2022-08-05 00:32:43 +0100

    Remove now-useless field

--- include/Hooks/NoteCutHapticEffectHook.hpp
+++ include/Hooks/NoteCutHapticEffectHook.hpp
@@ -10,8 +10,6 @@ using namespace Libraries::HM::HMLib::VR;
 namespace RumbleMod::Hooks {
     class NoteCutHapticEffectHook : IHook {
     public:
-        inline static SafePtr<HapticPresetSO> rumblePreset;
-
         void AddHooks() override;
         void RemoveHooks() override;
         explicit NoteCutHapticEffectHook(const std::string& name) : IHook(name) {}
